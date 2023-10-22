ecffde39aac7a3f280b03d8f71e82f06fb1f09ad
jeneboston <smilemusik2013@gmail.com>
2023-10-23 00:22:04 +0300

    123

--- src/main/java/de/ait/Main.java
+++ src/main/java/de/ait/Main.java
@@ -3,6 +3,6 @@ package de.ait;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Hi");
+        System.out.println("Hi-1");
     }
 }
