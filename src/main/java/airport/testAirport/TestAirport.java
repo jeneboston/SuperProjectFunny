33e5830c46ce5763c4a3c238272c3367f1f59a97
Євгеній Пінчук <evhenii.pinchuk@gmail.com>
2023-11-30 22:15:10 +0200

    еще !

--- src/main/java/airport/testAirport/TestAirport.java
+++ src/main/java/airport/testAirport/TestAirport.java
@@ -3,6 +3,6 @@ package airport.testAirport;
 public class TestAirport {
     public static void main(String[] args) {
 
-        System.out.println("hi Funny!!!");
+        System.out.println("hi Funny!!!!");
     }
 }
